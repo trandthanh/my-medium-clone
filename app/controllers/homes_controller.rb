97afa192659d5b7d0259ff5bb8752cd38c515e4f
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-05-25 23:44:22 +0200

    Allow users to create posts

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,5 @@
 class HomesController < ApplicationController
   def show
-
+    @posts = Post.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'homes#show'
+  resources :posts, only: [ :new, :create ]
 end
