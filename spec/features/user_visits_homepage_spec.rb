83d2866c1642fb5289882d34f11845ffd8afb601
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-05-25 22:56:50 +0200

    Add first feature spec

---
+++ spec/features/user_visits_homepage_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+RSpec.feature "User visits homepage" do
+  scenario "successfully and sees a logo" do
+    visit root_path
+    expect(page).to have_content "MyMediumClone"
+  end
+end
