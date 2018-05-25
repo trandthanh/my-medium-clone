bb7ba3fe1755c6092e587a376ef2a1fb75064844
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-05-26 01:20:12 +0200

    Add authorization to post creation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   validates :title, :body, presence: true
+
+  belongs_to :user
+
+  delegate :username, to: :user
 end
--- spec/features/user_creates_post_spec.rb
+++ spec/features/user_creates_post_spec.rb
@@ -1,7 +1,10 @@
 require "rails_helper"
 
 RSpec.describe "Creating post" do
+  let(:user) { User.create(username: "exampleuser", email: "example@user.com", password: "password", password_confirmation: "password") }
+
   scenario "successfully" do
+    sign_in user
     visit root_path
     click_on "Write a story"
 
@@ -9,10 +12,15 @@ RSpec.describe "Creating post" do
     fill_in "Body", with: "Some awesome content"
     click_on "Publish"
 
-    expect(page).to have_content "My first post"
+
+    within(".posts") do
+      expect(page).to have_content "My first post"
+      expect(page).to have_content "exampleuser"
+    end
   end
 
   scenario "unsuccessfully" do
+    sign_in user
     visit root_path
     click_on "Write a story"
 
@@ -21,5 +29,12 @@ RSpec.describe "Creating post" do
     click_on "Publish"
 
     expect(page).to have_css ".error"
+  end
+
+  scenario "non-logged in user cannot create post" do
+    visit root_path
+    click_on "Write a story"
+
+    expect(current_path).to eq(new_user_session_path)
   end
 end
