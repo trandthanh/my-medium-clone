81b7c3954686b82ce606bb83b1611fd8435dffdd
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-05-26 00:20:52 +0200

    fixing bug

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_05_25_211841) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
